e32a2c6b3cdee290b3d6cbd5dff805cf8d5609b7
robin <robin@jimdo.com>
2011-04-16 14:43:36 +0200

    gsdf

---
+++ js/jquery.launcher.js
@@ -0,0 +1,29 @@
+  (function($){
+    $.fn.launcher = function(options) {
+      return this.each(function() {
+	    var $this = $(this);
+        /*options = $.extend({
+            'maxLength': 100,
+			'align': 'right',
+			'valign': 'bottom',
+			'show': 'auto',
+			'elCount': 'auto'
+		}, options);*/
+        alert('ddd');
+        
+        
+        function blu() {
+          alert('blub');  
+        };
+				
+	  });
+    };
+
+   
+
+	
+  })(jQuery);
+  
+  
+  
+    
